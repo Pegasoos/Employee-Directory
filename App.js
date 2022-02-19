bad9effaa58b95864d48ded041c4725bffdb513f
Pegasoos <circumambulation24@yahoo.com>
2022-02-19 02:59:45 -0500

    Updates

--- App.js
+++
@@ -1,9 +0,0 @@
-import SearchResultsContainer from "./components/SearchResultsContainer.js";
-function App() {
-  return(
-  <div className = "container">
-     <SearchResultsContainer />
-  </div>
-  )
-}
-export default App;
--- utils/API.js
+++
@@ -1,8 +0,0 @@
-export default {
-
-    BASEURL:"https://randomuser.me/api/?results=5",
-
-    search: function(query){
-      return fetch(this.BASEURL)
-    }
-};
